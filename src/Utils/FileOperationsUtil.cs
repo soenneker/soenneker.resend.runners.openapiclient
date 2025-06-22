558baca31e1113a1c83ca7edc6aa17db1e83731e
Jake Soenneker <jake@soenneker.com>
2025-06-22 08:54:22 -0500

    Deps

--- src/Utils/FileOperationsUtil.cs
+++ src/Utils/FileOperationsUtil.cs
@@ -42,7 +42,7 @@ public sealed class FileOperationsUtil : IFileOperationsUtil
 
     public async ValueTask Process(CancellationToken cancellationToken = default)
     {
-        string gitDirectory = _gitUtil.CloneToTempDirectory($"https://github.com/soenneker/{Constants.Library.ToLowerInvariantFast()}");
+        string gitDirectory = await _gitUtil.CloneToTempDirectory($"https://github.com/soenneker/{Constants.Library.ToLowerInvariantFast()}", cancellationToken);
 
         string targetFilePath = Path.Combine(gitDirectory, "resend.yaml");
 
@@ -130,6 +130,6 @@ public sealed class FileOperationsUtil : IFileOperationsUtil
 
         string gitHubToken = EnvironmentUtil.GetVariableStrict("GH__TOKEN");
 
-        await _gitUtil.CommitAndPush(gitDirectory, "soenneker", "Jake Soenneker", "jake@soenneker.com", gitHubToken, "Automated update");
+        await _gitUtil.CommitAndPush(gitDirectory, "soenneker",  "jake@soenneker.com", gitHubToken, "Automated update", cancellationToken);
     }
 }
