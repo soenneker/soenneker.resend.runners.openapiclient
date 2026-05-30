783e89750703bdf6f5b8cee6e1f16529b3e3c8ce
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-05-29 19:29:42 -0500

    openapi fixer

--- src/Soenneker.Resend.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
+++ src/Soenneker.Resend.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
@@ -16,6 +16,7 @@ using Soenneker.OpenApi.Fixer.Abstract;
 using Soenneker.Utils.Directory.Abstract;
 using Soenneker.Utils.File.Abstract;
 using Soenneker.Utils.File.Download.Abstract;
+using Soenneker.Utils.Yaml.Abstract;
 using System.Collections.Generic;
 
 namespace Soenneker.Resend.Runners.OpenApiClient.Utils;
@@ -32,9 +33,10 @@ public sealed class FileOperationsUtil : IFileOperationsUtil
     private readonly IFileDownloadUtil _fileDownloadUtil;
     private readonly IFileUtil _fileUtil;
     private readonly IDirectoryUtil _directoryUtil;
+    private readonly IYamlUtil _yamlUtil;
 
     public FileOperationsUtil(ILogger<FileOperationsUtil> logger, IGitUtil gitUtil, IDotnetUtil dotnetUtil, IProcessUtil processUtil,
-        IFileDownloadUtil fileDownloadUtil, IFileUtil fileUtil, IDirectoryUtil directoryUtil, IKiotaUtil kiotaUtil, IOpenApiFixer openApiFixer)
+        IFileDownloadUtil fileDownloadUtil, IFileUtil fileUtil, IDirectoryUtil directoryUtil, IKiotaUtil kiotaUtil, IYamlUtil yamlUtil, IOpenApiFixer openApiFixer)
     {
         _logger = logger;
         _gitUtil = gitUtil;
@@ -45,6 +47,7 @@ public sealed class FileOperationsUtil : IFileOperationsUtil
         _fileDownloadUtil = fileDownloadUtil;
         _fileUtil = fileUtil;
         _directoryUtil = directoryUtil;
+        _yamlUtil = yamlUtil;
     }
 
     public async ValueTask Process(CancellationToken cancellationToken = default)
@@ -53,14 +56,17 @@ public sealed class FileOperationsUtil : IFileOperationsUtil
             cancellationToken: cancellationToken);
 
         string targetFilePath = Path.Combine(gitDirectory, "resend.yaml");
+        string jsonFilePath = Path.Combine(gitDirectory, "openapi.json");
 
         string fixedFilePath = Path.Combine(gitDirectory, "fixed.json");
 
         await _fileUtil.DeleteIfExists(targetFilePath, cancellationToken: cancellationToken);
+        await _fileUtil.DeleteIfExists(jsonFilePath, cancellationToken: cancellationToken);
 
         string? filePath = await _fileDownloadUtil.Download("https://raw.githubusercontent.com/resend/resend-openapi/refs/heads/main/resend.yaml",
             targetFilePath, fileExtension: ".yaml", cancellationToken: cancellationToken);
-        await _openApiFixer.Fix(targetFilePath, fixedFilePath, cancellationToken);
+        await _yamlUtil.SaveAsJson(filePath ?? targetFilePath, jsonFilePath, true, cancellationToken);
+        await _openApiFixer.Fix(jsonFilePath, fixedFilePath, cancellationToken);
 
 
         await _kiotaUtil.EnsureInstalled(cancellationToken);
